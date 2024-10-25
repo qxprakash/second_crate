dafedf4cbc110fd5fa114ab8e384c1dad36d5164
Prakash <prakashope@gmail.com>
2024-10-26 00:04:53 +0530

    changes

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@
 //! example_to_embed();
 //! ```
 
-// Set up git fallback
+// Set up git fallback url
 docify_clone::set_git_fallback!("https://gitlab.com/prakashh21/first_crate.git", "src/lib.rs");
 
 #[doc = docify_clone::embed!("first_crate/src/lib.rs", example_to_embed)]
